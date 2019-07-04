a4739f93330bf0b342e2f6dc497ffe22bdd0f38f
Tom Howard <tom@windyroad.com.au>
2019-07-04 17:50:00 +1000

    trying to get inspectlet working

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,6 +23,57 @@ class Layout extends React.Component {
     this.timeoutId = setTimeout(() => {
       this.setState({ loading: '' });
     }, 100);
+
+    (function() {
+      console.log('inspectletjs');
+      if (
+        typeof window !== 'undefined' &&
+        document.location.hostname == 'windyroad.com.au'
+      ) {
+        const insp_ab_loader = true; // set this boolean to false to disable the A/B testing loader
+        window.__insp = window.__insp || [];
+        window.__insp.push(['wid', 1654706623]);
+        const ldinsp = function() {
+          if (typeof window.__inspld !== 'undefined') return;
+          window.__inspld = 1;
+          const insp = document.createElement('script');
+          insp.type = 'text/javascript';
+          insp.async = true;
+          insp.id = 'inspsync';
+          insp.src = `${
+            document.location.protocol == 'https:' ? 'https' : 'http'
+          }://cdn.inspectlet.com/inspectlet.js?wid=1654706623&r=${Math.floor(
+            new Date().getTime() / 3600000,
+          )}`;
+          const x = document.getElementsByTagName('script')[0];
+          x.parentNode.insertBefore(insp, x);
+          if (typeof insp_ab_loader !== 'undefined' && insp_ab_loader) {
+            const adlt = function() {
+              const e = document.getElementById('insp_abl');
+              if (e) {
+                e.parentNode.removeChild(e);
+                window.__insp.push(['ab_timeout']);
+              }
+            };
+            const adlc = 'body{ visibility: hidden !important; }';
+            const adln =
+              typeof window.insp_ab_loader_t !== 'undefined'
+                ? window.insp_ab_loader_t
+                : 1200;
+            insp.onerror = adlt;
+            const abti = setTimeout(adlt, adln);
+            window.__insp_abt = abti;
+            const abl = document.createElement('style');
+            abl.id = 'insp_abl';
+            abl.type = 'text/css';
+            if (abl.styleSheet) abl.styleSheet.cssText = adlc;
+            else abl.appendChild(document.createTextNode(adlc));
+            document.head.appendChild(abl);
+          }
+        };
+        setTimeout(ldinsp, 0);
+      }
+    })();
   }
 
   componentWillUnmount() {
@@ -56,62 +107,6 @@ class Layout extends React.Component {
             <Helmet>
               <title>Addressr by Mountain Pass</title>
               <meta name="description" content="Addressr by Mountain Pass" />
-              <script type="text/javascript" id="inspectletjs">
-                {(function() {
-                  if (
-                    typeof window !== 'undefined' &&
-                    document.location.hostname == 'windyroad.com.au'
-                  ) {
-                    const insp_ab_loader = true; // set this boolean to false to disable the A/B testing loader
-                    window.__insp = window.__insp || [];
-                    window.__insp.push(['wid', 1654706623]);
-                    const ldinsp = function() {
-                      if (typeof window.__inspld !== 'undefined') return;
-                      window.__inspld = 1;
-                      const insp = document.createElement('script');
-                      insp.type = 'text/javascript';
-                      insp.async = true;
-                      insp.id = 'inspsync';
-                      insp.src = `${
-                        document.location.protocol == 'https:'
-                          ? 'https'
-                          : 'http'
-                      }://cdn.inspectlet.com/inspectlet.js?wid=1654706623&r=${Math.floor(
-                        new Date().getTime() / 3600000,
-                      )}`;
-                      const x = document.getElementsByTagName('script')[0];
-                      x.parentNode.insertBefore(insp, x);
-                      if (
-                        typeof insp_ab_loader !== 'undefined' &&
-                        insp_ab_loader
-                      ) {
-                        const adlt = function() {
-                          const e = document.getElementById('insp_abl');
-                          if (e) {
-                            e.parentNode.removeChild(e);
-                            window.__insp.push(['ab_timeout']);
-                          }
-                        };
-                        const adlc = 'body{ visibility: hidden !important; }';
-                        const adln =
-                          typeof window.insp_ab_loader_t !== 'undefined'
-                            ? window.insp_ab_loader_t
-                            : 1200;
-                        insp.onerror = adlt;
-                        const abti = setTimeout(adlt, adln);
-                        window.__insp_abt = abti;
-                        const abl = document.createElement('style');
-                        abl.id = 'insp_abl';
-                        abl.type = 'text/css';
-                        if (abl.styleSheet) abl.styleSheet.cssText = adlc;
-                        else abl.appendChild(document.createTextNode(adlc));
-                        document.head.appendChild(abl);
-                      }
-                    };
-                    setTimeout(ldinsp, 0);
-                  }
-                })()}
-              </script>
             </Helmet>
 
             <div id="main" className="alt">
