ea10a88017a0c54524f28b314604d34ea842a3c9
Tom Howard <tom@windyroad.com.au>
2020-02-13 20:50:20 +1100

    removed mongo requriement

--- src/pages/quick-start.js
+++ src/pages/quick-start.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import howitworks from '../assets/images/addressr.svg';
 import Banner from '../components/Banner';
 import Layout from '../components/layout';
 import { getProfile } from '../utils/auth';
@@ -43,23 +44,11 @@ docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elas
                 </pre>
               </li>
               <li>
-                Start mongodb. For example
-                <pre>
-                  <code>
-                    {`docker pull mongo:4.0.11 mkdir ~/data
-docker run -d -p 27017:27017 -v ~/data:/data/db MONGO_INITDB_ROOT_USERNAME=root MONGO_INITDB_ROOT_PASSWORD=example mongo:4.0.11`}
-                  </code>
-                </pre>
-              </li>
-              <li>
                 Start API server
                 <pre>
                   <code>
                     {`export ELASTIC_PORT=9200
 export ELASTIC_HOST=localhost
-export MONGO_USERNAME=root
-export MONGO_PASSWORD=example
-export MONGO_URL=mongodb://localhost:27017
 addressr-server`}
                   </code>
                 </pre>
@@ -70,9 +59,6 @@ addressr-server`}
                   <code>
                     {`export ELASTIC_PORT=9200
 export ELASTIC_HOST=localhost
-export MONGO_USERNAME=root
-export MONGO_PASSWORD=example
-export MONGO_URL=mongodb://localhost:27017
 addressr-loader`}
                   </code>
                 </pre>
@@ -99,66 +85,18 @@ addressr-loader`}
                 addressr regularly updated
               </li>
             </ol>
-            {/* <h2>How it Works</h2>
-            <pre>
-              <code>
-                {`                             ┌──────────────────┐
-                1. Get       │                  │
-          ┌──────G-NAF──────>│   data.gov.au    │
-          │                  │                  │
-          │                  └──────────────────┘
-          │                       .─────────.
-          │                      (           )
- ┌────────────────┐              │\`─────────'│
- │................│ 2. Save and  │           │
- │.address-loader.│───extract───>│filesystem │
- │................│    G-NAF     │           │
- └────────────────┘              └           ┘
-          │                       \`─────────'
-          │                       .─────────.
-          │                      (           )
-          │                      │\`─────────'│              ┌───────────────────────┐
-          │      3a. Index       │  elastic  │     Search   │.......................│
-          ├────────G-NAF────────>│  search   │<──Addresses──│<───────────────────┐..│
-          │       records        │           │              │....................│..│
-          │                      └           ┘              │....................│..│
-          │                       \`─────────'               │....................│..│
-          │                       .─────────.               │....addressr-server.│..│
-          │                      (           )              │....................│..│
-          │                      │\`─────────'│              │....................│..│
-          │      3b. Store       │           │      Get     │....................│..│
-          └────────G-NAF────────>│  mongodb  │<───Address───│<──┐................│..│
-                  Records        │           │    Details   │...│................│..│
-                                 └           ┘              └───┴────────────────┴──┘
-                                  \`─────────'                   ^                ^
-                                                                │                │
-                                                               GET               │
-                                                         /addresses/(ID)         │
-                                                                │               GET
-                                                                │        /addresses?q=...
-                                                                │                │
-                                                             ┌──────────────────────┐
-                                                             │                      │
-                                                             │                      │
-                                                             │        client        │
-                                                             │                      │
-                                                             │                      │
-                                                             └──────────────────────┘
-`}
-              </code>
-            </pre> */}
+            <h2>How it works</h2>
+            <img
+              src={howitworks}
+              alt="how it works"
+              style={{
+                border: 'solid white 20px',
+              }}
+            />
 
             <h2>System requirements</h2>
-            <p>Node JS &gt;= 11.14.0</p>
             <h3>Elastic Search:</h3>
             <p>elasticsearch-oss &gt;= 7.2.0 with 1.4GiB of memory</p>
-            <h3>Mongo DB</h3>
-            <p>mongo &gt;= 4.0.11 with 3.4GiB of memory</p>
-            <p>
-              Mongo&apos;s memory usage is complicated and the amount it uses
-              depends on how much is available. In our experience, the more
-              memory it had available the faster the data load would run.
-            </p>
             <h3>Addressr Loader</h3>
             <p>Node JS &gt;= 11.14.0 with 1GiB of memory</p>
             <h3>Addressr Server</h3>
