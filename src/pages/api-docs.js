48504495aeaf2ae174b1707d9a9df8ed8a229b4d
Tom Howard <tom.howard@greater.com.au>
2023-09-18 00:02:25 +1000

    fixed build

--- src/pages/api-docs.js
+++ src/pages/api-docs.js
@@ -2,29 +2,34 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Layout from '../components/layout';
 import spec from '../swagger.yaml';
-import SwaggerUI from "swagger-ui-react"
+//import SwaggerUI from 'swagger-ui'
 import "swagger-ui-react/swagger-ui.css"
 
-const ApiDocs = () => {
-  return (
-    <Layout>
-      <Helmet>
-        <title>API Docs - Addressr by Mountain Pass</title>
-        <meta
-          name="description"
-          content="API Docs for Addressr by Mountain Pass"
-        />
-      </Helmet>
-      <div className="swagger-wrapper">
-        <SwaggerUI spec={spec} docExpansion="full" supportedSubmitMethods={[]}/>
-        {/* <SwaggerUI
-          spec={spec}
-          docExpansion="full"
-          supportedSubmitMethods={[]}
-        /> */}
-      </div>
-    </Layout>
-  );
-};
+class ApiDocs extends React.Component {
+  componentDidMount() {
+    window.SwaggerUI({
+        dom_id: '#swagger',
+        spec,
+        docExpansion: "full",
+        supportedSubmitMethods: []
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <Helmet>
+          <title>API Docs - Addressr by Mountain Pass</title>
+          <meta
+            name="description"
+            content="API Docs for Addressr by Mountain Pass"
+          />
+        </Helmet>
+        <div className="swagger-wrapper">
+          <div id="swagger" />
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default ApiDocs;
