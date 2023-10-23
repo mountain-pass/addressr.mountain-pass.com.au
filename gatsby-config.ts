09091bc17ea462ebbe30bb0b3066c1bb98d55bf1
Tom Howard <tom.howard@greater.com.au>
2023-10-24 07:55:39 +1100

    testing home fixes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `addressr.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    author: 'Mountain Pass',
+    description: 'Australian Address Validation and Autocomplete',
+    siteUrl: `https://addressr.io`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -12,23 +14,29 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      icon: 'src/assets/images/addressr-square.png', // This path is relative to the root of the site.
     }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-force-trailing-slashes`,
+      // options: {
+      //   excludedPaths: [],
+      // },
+    }]
 };
 
 export default config;
---
+++ gatsby-node.js
@@ -0,0 +1,73 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+exports.onCreatePage = async ({ actions }) => {
+  const { createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `/signup`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `/quick-start/`,
+  });
+
+  createRedirect({
+    fromPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `/home/`,
+  });
+
+  createRedirect({
+    fromPath: `/signup/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `/quick-start/`,
+  });
+
+  createRedirect({
+    fromPath: `/community-support`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `https://gitter.im/mountainpass-addressr/community/~embed`,
+  });
+
+  createRedirect({
+    fromPath: `/community-support/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+    toPath: `https://gitter.im/mountainpass-addressr/community/~embed`,
+  });
+
+};
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    /*
+     * During the build step, `auth0-js` will break because it relies on
+     * browser-specific APIs. Fortunately, we don’t need it during the build.
+     * Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+     * during the build. (See `src/utils/auth.js` to see how we prevent this
+     * from breaking the app.)
+     */
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /auth0-js/,
+            use: loaders.null(),
+          },
+          {
+            test: /swagger-ui/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
