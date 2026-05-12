c1b8cd94faf9c0a1589fb8d4e5e12bd53005026d
Tom Howard <tom@windyroad.com.au>
2026-05-12 22:00:20 +1000

    renable

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'gatsby';
-import React from 'react';
 import Helmet from 'react-helmet';
 import pic01 from '../assets/images/pic01.jpg';
 import pic02 from '../assets/images/pic02.jpg';
@@ -44,7 +43,7 @@ const HomeIndex = () => {
         </div>
       </Banner>
 
-      {/* <div>
+      <div>
         <section id="zero" style={{ padding: '2em 3em 2em 3em' }}>
           <article>
             <div className="content">
@@ -52,7 +51,7 @@ const HomeIndex = () => {
             </div>
           </article>
         </section>
-      </div> */}
+      </div>
 
       <div id="main">
         <section id="one" className="tiles">
